837a83c4e4bd63dd070849551344f13385013c25
unknown <Ahmad.R1911@Gmail.com>
2021-07-16 17:41:17 +0430

    change ui and create class tblmetadata
    change layout display name and directrion,
    create metadata class for show title in persian name

---
+++ PhoneBook/PhoneBook/Models/EntityModels/TblUserMetaData.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PhoneBook.Models.EntityModels
+{
+    public class TblUserMetaData
+    {
+        [Display]
+        public int ID { get; set; }
+        [Display(Name ="نام")]
+        public string Firstname { get; set; }
+        [Display(Name = "نام خانوادگی")]
+        public string Lastname { get; set; }
+        [Display(Name = "موبایل")]
+        public string Mobile { get; set; }
+        [Display(Name = "تلفن ثابت")]
+        public string PhoneNumber { get; set; }
+        [Display(Name = "کد ملی")]
+        public string NationalCode { get; set; }
+        [Display(Name = "گروه")]
+        public string cGroup { get; set; }
+        [Display(Name = "عکس")]
+        public byte[] Picture { get; set; }
+    }
+}
+
+namespace PhoneBook.Models.DomainModel
+{
+    [MetadataType(typeof(EntityModels.TblUserMetaData))]
+    public partial class TblUser
+    {
+
+    }
+}
